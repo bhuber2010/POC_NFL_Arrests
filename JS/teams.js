37d27ae6c9fd07631c23ae9ff11b8e35e816402b
bhuber2010 <bhuber@alumni.colostate.edu>
2015-11-06 22:10:13 -0700

    smaple js for patrios info on Team page

--- JS/teams.js
+++ JS/teams.js
@@ -1,4 +1,14 @@
-$.get( "http://nflarrest.com/api/v1/team/arrests/NE", function( data ) {
-  $('.data').
-  console.log(data);
-});
+// $.get("http://nflarrest.com/api/v1/team/arrests/NE", function(data) {
+//   $( ".data" )
+//     .append("Name: " + data[0].Name + " ")
+//     .append("Position: " + data[0].Position + " ")
+//     .append("Category: " + data[0].Category);
+// }, "json" );
+
+var $player = $('.team-list')
+
+$.get("http://nflarrest.com/api/v1/team/arrests/NE", function(player) {
+  $.each(player, function(i,player){
+    $player.append("<tr><td class='player-name'>" + player.Name + "</td><td class='player-position'>" + player.Position + "</td><td class='player-crime'>" + player.Category + "</td></tr>")
+  })
+}, "json" );
