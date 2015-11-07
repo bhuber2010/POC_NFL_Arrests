0e1482e3a1481cbf6f5342c32e26994640b0fc8e
bhuber2010 <bhuber@alumni.colostate.edu>
2015-11-07 15:31:16 -0700

    Teams page good to go, teams.js loops all teams

--- JS/teams.js
+++ JS/teams.js
@@ -5,16 +5,34 @@
 //     .append("Category: " + data[0].Category);
 // }, "json" );
 
-var $player = $('.team-list')
+var $player = $('.team-list');
+var $teamPanel = $('.panel-group')
 
-$.get("http://nflarrest.com/api/v1/team/arrests/NE", function(player) {
-  $.each(player, function(i,player){
-    $player.append(
-      "<tr><td class='player-name'>" + player.Name +
-      "</td><td class='player-position'>" + player.Position +
-      "</td><td class='player-crime'>" + player.Category +
-      "</td><td class='player-date'>" + player.Date +
-      "</td></tr>"
-    )
-  })
-}, "json" );
+var $teams = $.get("http://nflarrest.com/api/v1/team", function(team) {
+              $.each(team, function(i,team){
+                $teamPanel.append(
+                  "<div class='panel panel-default'><div class='panel-heading' data-id=" + team.Team +
+                  "><a><h3 class='panel-title collapsed' data-parent='#accordion' data-toggle='collapse' data-target=#" + team.Team +
+                  " href=#" + team.Team + ">" + team.Team + "<span> : </span>" + "<span class='badge'>" + team.arrest_count + "</span></h3></a></div><div id=" + team.Team +
+                  " class='panel-collapse collapse'><table class='table table-striped table-responsive team-list'>" +
+                  "<thead><th>Name</th><th>Position</th><th>Crime</th><th>Date</th></thead></table></div></div>"
+                )
+              })
+            }, "json" );
+
+$teams.done(function(teamList){
+  console.log(teamList[0].Team);
+  $.each(teamList, function(t,teamList) {
+    $.get("http://nflarrest.com/api/v1/team/arrests/" + teamList.Team, function(player) {
+      $.each(player, function(p,player){
+        $("#" + teamList.Team + " table").append(
+          "<tr><td class='player-name'>" + player.Name +
+          "</td><td class='player-position'>" + player.Position +
+          "</td><td class='player-crime'>" + player.Category +
+          "</td><td class='player-date'>" + player.Date +
+          "</td></tr>"
+        )
+      })
+      }, "json" );
+    })
+});
